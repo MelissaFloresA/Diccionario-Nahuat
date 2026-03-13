99d6027117eb6b21a94a4f13997e1f1f24a95075
melissa.flores <melissa.abi.flores@gmail.com>
2026-03-12 20:13:48 -0600

    Navegation var using Fragment´s

--- app/src/main/java/com/example/diccionario/MainActivity.kt
+++ app/src/main/java/com/example/diccionario/MainActivity.kt
@@ -1,20 +1,43 @@
 package com.example.diccionario
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        // Fragment inicial
+        replaceFragment(InicioFragment())
+
+        bottomNavigation.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.nav_home -> replaceFragment(InicioFragment())
+
+                R.id.nav_search -> replaceFragment(BusquedaFragment())
+
+                R.id.nav_favorites -> replaceFragment(FavoritosFragment())
+            }
+
+            true
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
+
     }
 }
